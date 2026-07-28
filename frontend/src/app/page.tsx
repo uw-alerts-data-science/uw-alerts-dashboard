01563a6f60c1655dea40f11c834f3c11ad8a2200
aschnarre <aschn@AUSTINS_PC>
2026-07-27 17:10:45 -0700

    Updated values for testing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import MapLibreMap from "../components/MapLibreMap";
 
-//Max hours is 999 for testing
+//Max hours is 720 for testing
 export default function HomePage() {
-  return <MapLibreMap recentHours={6} />;
+  return <MapLibreMap recentHours={720} />;
 }
