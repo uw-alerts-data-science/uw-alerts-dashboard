bfd853f07a6519fc1323fa355730bec8e19bf6e4
aschnarre <aschn@AUSTINS_PC>
2026-08-20 18:34:50 -0700

    Update Recent alerts time window

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,5 +2,5 @@ import MapLibreMap from "../components/MapLibreMap";
 
 //Max hours is 720 for testing
 export default function HomePage() {
-  return <MapLibreMap recentHours={720} />;
+  return <MapLibreMap recentHours={6} />;
 }
